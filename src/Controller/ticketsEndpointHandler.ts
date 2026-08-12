0d2f481fdeb9e7221609b28af56da77337c61233
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-08-12 11:51:06 +0530

    chore:stop tracking tickts.json database file

--- src/Controller/ticketsEndpointHandler.ts
+++ src/Controller/ticketsEndpointHandler.ts
@@ -1,5 +1,12 @@
 import { type Request, type Response } from "express";
-import { getAllTicket, postTicket } from "../Services/ticketServices.js";
+import {
+  getAllTicket,
+  postTicket,
+  viewSpecificTicket,
+  updateStatus,
+  assignTicket,
+  deleteTick,
+} from "../Services/ticketServices.js";
 import type { TicketInput } from "../types/types";
 import { ValidationError, NotFoundError } from "../Services/ticketServices.js";
 
@@ -17,6 +24,59 @@ export async function enterTicket(req: Request, res: Response) {
     if (err instanceof ValidationError)
       res.status(err.statusCode).json({ error: err.message });
     if (err instanceof Error) res.status(400).json({ error: err.message });
+    throw err;
+  }
+}
+
+export async function getSpecificTicketInformation(
+  req: Request,
+  res: Response,
+) {
+  try {
+    const ticketId = Number(req.params.ticketId);
+    const ticket = await viewSpecificTicket(ticketId);
+    res.status(200).json(ticket);
+  } catch (error: unknown) {
+    if (error instanceof NotFoundError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    }
+    throw error;
+  }
+}
+
+export async function statusUpdateController(req: Request, res: Response) {
+  try {
+    const id = Number(req.params.ticketId);
+    const { status, assignee } = req.body;
+    if (status && status.trim() !== "") await updateStatus(id, status);
+    else if (assignee && assignee.trim() !== "")
+      await assignTicket(id, assignee);
+    else throw new Error("Request body cannot be empty");
+    res.status(200).json({ success: "Updated successfully" });
+  } catch (err: unknown) {
+    if (err instanceof ValidationError || err instanceof NotFoundError)
+      return res.status(err.statusCode).json({ error: err.message });
+    if (err instanceof Error)
+      return res.status(400).json({ error: err.message });
+    throw err;
+  }
+}
+
+export async function deleteTicketController(req: Request, res: Response) {
+  try {
+    const id = Number(req.params.ticketId);
+    await deleteTick(id);
+    res.status(202).json({ success: true, message: "deleted successfully" });
+  } catch (err: unknown) {
+    if (err instanceof NotFoundError)
+      return res
+        .status(err.statusCode)
+        .json({ success: false, message: err.message });
+    if (err instanceof Error)
+      return res.status(400).json({ success: false, message: err.message });
     throw err;
   }
 }
--- src/Routes/ticketRoutes.ts
+++ src/Routes/ticketRoutes.ts
@@ -2,11 +2,20 @@ import express from "express";
 import {
   getTicketDetails,
   enterTicket,
+  getSpecificTicketInformation,
+  statusUpdateController,
+  deleteTicketController,
 } from "../Controller/ticketsEndpointHandler.js";
 
 const router = express.Router();
 
 router.get("/", getTicketDetails);
 router.post("/", enterTicket);
+
+router.get("/:ticketId", getSpecificTicketInformation);
+
+router.patch("/:ticketId", statusUpdateController);
+
+router.delete("/:ticketId", deleteTicketController);
 
 export default router;
