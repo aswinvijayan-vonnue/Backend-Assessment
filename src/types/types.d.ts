499ec6d63c1e7c6530728068bb35ba64ed195bca
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-08-12 10:04:29 +0530

    completed service section and validation of user input

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,16 +1,17 @@
-export interface Ticket{
-    id:number;
-    title:string;
-    description:string;
-    priority:'Low'|'Medium'|'High';
-    status:'Pending'|'Completed';
-    assignee?:string;
+export type StatusType = "Pending" | "Completed";
+export type priorityType = "Low" | "Medium" | "High";
+export interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+  priority: priorityType;
+  status: StatusType;
+  assignee?: string;
 }
 
-export interface TicketInput{
-    title:string;
-    description:string;
-    priority:'Low'|'Medium'|'High';
-    status:'Pending'|'Completed';
-    
+export interface TicketInput {
+  title: string;
+  description: string;
+  priority: "Low" | "Medium" | "High";
+  status: "Pending" | "Completed";
 }
